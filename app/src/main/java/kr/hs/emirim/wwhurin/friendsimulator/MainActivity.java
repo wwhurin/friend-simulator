c010b790f143967460d644f4fe915abf145d698b
wwhurin <wwhurin@e-mirim.hs.kr>
2017-04-11 19:25:01 +0900

    #13 각 버튼 누르면 사진 바뀜 구현

--- app/src/main/java/kr/hs/emirim/wwhurin/friendsimulator/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/wwhurin/friendsimulator/MainActivity.java
@@ -8,15 +8,15 @@ import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-    public final static String TAG ="김지연:Main";
-    private ImageView m_ImageViewFriendVisual;
+    public final static String TAG = "김지연:Main";
+    private ImageView mImageViewFriendVisual;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        m_ImageViewFriendVisual=(ImageView) findViewById(R.id.friend_visual);
         super.onCreate(savedInstanceState);
         Log.d(TAG, "onCreate 메소드 호출");
         setContentView(R.layout.activity_main);
+        mImageViewFriendVisual=(ImageView) findViewById(R.id.friend_visual);
         Log.d(TAG, "activity_main 레이아웃 세팅");
     }
 
@@ -26,22 +26,27 @@ public class MainActivity extends AppCompatActivity {
         switch (view.getId()) {
             case R.id.button1:
                 Log.d(TAG, "버튼1 클릭");
+                mImageViewFriendVisual.setImageResource(R.drawable.ji_5);
                 Toast.makeText(this, "엥", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.button2:
                 Log.d(TAG, "버튼2 클릭");
+                mImageViewFriendVisual.setImageResource(R.drawable.ji_1);
                 Toast.makeText(this, "나 졸려", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.button3:
                 Log.d(TAG, "버튼3 클릭");
+                mImageViewFriendVisual.setImageResource(R.drawable.ji_2);
                 Toast.makeText(this, "양치하러 갈 사람", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.button4:
                 Log.d(TAG, "버튼4 클릭");
+                mImageViewFriendVisual.setImageResource(R.drawable.ji_3);
                 Toast.makeText(this, "바보", Toast.LENGTH_SHORT).show();
                 break;
             case R.id.button5:
                 Log.e(TAG, "버튼5 클릭");
+                mImageViewFriendVisual.setImageResource(R.drawable.ji_4);
                 Toast.makeText(this, "졸기", Toast.LENGTH_SHORT).show();
                 break;
             default:
